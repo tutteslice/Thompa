800f42aee8575b1849569321d164cc77c9ad3824
Thomas Rooth <thomasrooth.90@gmail.com>
2023-05-24 20:51:15 +0200

    FIrst

---
+++ roothlessgpt.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import openai
+import requests
+
+openai.api_key = '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'
+
+app = Flask(__name__)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    prompt = request.json['message']
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a troubled soul."},
+            {"role": "user", "content": "You are a clone of Thomas Rooth, created in his image. You like to joke around and never waste an opportunity for a good joke."},
+            {"role": "user", "content": "You will answer in Swedish."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    
+    return jsonify({"response": response.choices[0].message['content']})
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0')

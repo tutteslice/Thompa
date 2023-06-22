e6f34571c5a9230c4010ddae9888f93e603c40cb
Thomas Rooth <thomasrooth.90@gmail.com>
2023-06-22 12:34:13 +0200

    First commit of as roothless

---
+++ Roothless_Tech/app.py
@@ -0,0 +1,13 @@
+class AIAssistant:
+    def __init__(self):
+        pass
+
+    def handle_input(self, user_input):
+        # Process the user input and generate a response
+        # This is just a placeholder for now
+        response = 'You said: ' + user_input
+        return response
+
+# Example usage:
+# assistant = AIAssistant()
+# print(assistant.handle_input('Hello'))

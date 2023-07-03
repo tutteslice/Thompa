91f4e91aba32b433dc87a2a2b34aff6552c7ca09
tutteslice <thomasrooth.90@gmail.com>
2023-07-03 21:45:57 +0200

    gogogo

--- Roothless_Tech/app.py
+++ Roothless_Tech/app.py
@@ -8,7 +8,7 @@ print(secrets.token_hex(16))
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key'  # replace with your secret key
+app.secret_key = os.getenv('SECRET_KEY')
 
 # Load chat logs and bio data at the start of the application
 with open('Roothless_Tech/JSON/chatlog.json') as f:
